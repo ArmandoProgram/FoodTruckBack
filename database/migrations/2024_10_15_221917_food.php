fe4fd82280a8d7cf9ee7bf350837a1890242606a
ArmandoProgram <ArmandoProgram@users.noreply.github.com>
2024-11-18 14:50:36 -0600

    moviles

--- database/migrations/2024_10_15_221917_food.php
+++ database/migrations/2024_10_15_221917_food.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('foods', function (Blueprint $table) {
+        Schema::create('food', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->text('description');
             $table->float('price');
-            $table->text('url_image');
+            $table->text('url_img');
             $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
